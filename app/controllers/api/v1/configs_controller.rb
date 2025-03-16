333732a1a6d8dd1cfbc1b036052bbe8780af734f
axel alexander <axel_nomore@hotmail.com>
2025-03-16 14:26:38 -0300

    refactor(db) set config db v9

---
+++ app/controllers/api/v1/configs_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  class ConfigsController < Api::ApplicationApiController
+    def index
+      configs = {
+        rails_max_threads: ENV.fetch("RAILS_MAX_THREADS") { 5 },
+        # Adicione outras configurações aqui
+        database_host: ENV['DATABASE_HOST'],
+        database_port: ENV['DATABASE_PORT'],
+        # ... outras variáveis de ambiente ...
+      }
+
+      render json: configs
+    end
+  end
+end
